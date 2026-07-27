e8e604d0b6f5b32d6ad5a4767fe7c7450e0ac597
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-07-27 17:23:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ function updateScore(team1, score1, team2, score2) {
 
 // Egzanp mizajou match
 updateScore("Haiti", 1, "Maroc", 0);
+function showPage(page) {
+
+    if(page === "Match"){
+        alert("Tout match yo");
+    }
+
+    if(page === "Live"){
+        alert("Match an dirèk 🔴");
+    }
+
+    if(page === "Klasman"){
+        alert("Klasman ekip yo 🏆");
+    }
+
+    if(page === "Favori"){
+        alert("Match favori yo ⭐");
+    }
+
+}
