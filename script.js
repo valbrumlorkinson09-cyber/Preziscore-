821dd59700cb88f31c306ae3ef3af625904a65e2
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-07-27 14:44:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// FIA Score Live - premye fonksyon yo
 
+console.log("FIA Score Live pare!");
+
+function updateScore(team1, score1, team2, score2) {
+    console.log(team1 + " " + score1 + " - " + score2 + " " + team2);
+}
+
+// Egzanp mizajou match
+updateScore("Haiti", 1, "Maroc", 0);
