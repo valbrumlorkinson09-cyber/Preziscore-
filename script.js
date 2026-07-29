a129db1dc6b43b735335cb227dc21bf621197357
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-07-29 15:54:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
 
+// ===============================
+// PREZISCORE MAIN SCRIPT
+// ===============================
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+
+console.log("⚽ PreziScore lanse avèk siksè");
+
+
+
+// BOUTON HERO
+
+const liveButton = document.querySelector(".hero button");
+
+
+if(liveButton){
+
+liveButton.addEventListener("click", function(){
+
+window.location.href = "live.html";
+
+});
+
+}
+
+
+
+// ANIMATION CARDS
+
+const cards = document.querySelectorAll(".card");
+
+
+cards.forEach(card=>{
+
+card.addEventListener("mouseenter",()=>{
+
+card.style.transform="translateY(-8px)";
+card.style.transition="0.3s";
+
+});
+
+
+card.addEventListener("mouseleave",()=>{
+
+card.style.transform="translateY(0)";
+
+});
+
+
+});
+
+
+
+// MESSAGE DEMARAJ
+
+setTimeout(()=>{
+
+console.log("🔥 Byenveni sou PreziScore Football Intelligence");
+
+},1000);
+
+
+
+});
