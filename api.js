63ccc6f7a77e3ae9901d55f082786ee2762f491c
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-07-29 18:51:17 -0400

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,36 @@
 
+// ===============================
+// PREZISCORE CONFIGURATION
+// ===============================
+
+
+// API FOOTBALL CONFIG
+
+const API_CONFIG = {
+
+    BASE_URL: "https://v3.football.api-sports.io",
+
+    API_KEY: "47f671279defefb2b169097f1062a2a6:
+
+};
+
+
+
+// ===============================
+// APP CONFIG
+// ===============================
+
+
+const APP_CONFIG = {
+
+    NAME: "PreziScore",
+
+    VERSION: "1.0.0",
+
+    LANGUAGE: "ht"
+
+};
+
+
+
+console.log("⚽ PreziScore Config Loaded");
